fb6e3cdf888ef5c5f67eae9e69021890f1f51589
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-10-26 15:30:15 +0200

    Create piedra_papel_tijera.cpp

---
+++ piedra_papel_tijera.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+string computerOption(){
+    srand(static_cast<unsigned int>(time(nullptr)));
+    //lista de palabras
+    vector<string> palabras = {
+        "piedra", "papel", "tijera"
+    };
+    // indice aleatorio
+    int indiceAleatorio = rand() % palabras.size();
+    // Obtiene la palabra aleatoria
+    string palabraAleatoria = palabras[indiceAleatorio];
+
+    return palabraAleatoria;
+}
+
+bool included(string elemento){
+    bool esta_presente = false;
+    vector<string> opciones = {"piedra","papel","tijera"};
+    for (int i = 0; i < opciones.size(); i++){
+        if (opciones[i] == elemento) {
+            esta_presente = true;
+            break;
+        }
+    }
+    return esta_presente;
+}
+
+string enterOption(){
+    bool esta;
+    string option;
+    cout << "Piedra, Papel o tijera: "; cin >> option;
+
+    return option;
+}
+
+int main()
+{
+    bool inclu;
+    string opcion;
+    string opcion_compu;
+
+    opcion = enterOption();
+    inclu = included(opcion);
+    opcion_compu = computerOption();
+
+    if(inclu == true){
+        cout << "TU OPCION: " << opcion << endl;
+        cout << "OPCION COMPU: " << opcion_compu << endl;
+        if(opcion != opcion_compu){
+            if(opcion == "piedra" && opcion_compu == "papel"){
+                cout << "\nPERDISTE: papel envuelve piedra" << endl;
+            }else if(opcion == "piedra" && opcion_compu == "tijera"){
+                cout << "\nGANASTE: piedra machaca tijera" << endl;
+            }else if(opcion == "papel" && opcion_compu == "piedra"){
+                cout << "\nGANASTE: papel envuelve piedra" << endl;
+            }else if(opcion == "papel" && opcion_compu == "tijera"){
+                cout << "\nPERDISTE: tijera corta papel" << endl;
+            }else if(opcion == "tijera" && opcion_compu == "piedra"){
+                cout << "\nPERDISTE: piedra machaca tijera" << endl;
+            }else{
+                cout << "\nGANASTE: tijera corta papel" << endl;
+            }
+        }else{
+            cout << "\nEMPATE" << endl;
+        }
+    }else{
+        cout << "\nOPCION NO VALIDA" << endl;
+    };
+    return 0;
+}
